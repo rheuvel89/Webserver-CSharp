e001830aea32574a6798e5385717df17206307fa
Tos Alles <talles@sogyo.nl>
2015-10-05 14:23:54 +0200

    Alle voorbeeldcode uit de case

---
+++ webserver-csharp/Interfaces.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace nl.sogyo.webserver
+{
+	public enum HttpStatusCode {
+		OK = 200, NotFound = 404, ServerError = 500
+	}
+
+	public enum HttpMethod {GET, POST}
+
+	public interface Request {
+		HttpMethod getHTTPMethod();
+		string getResourcePath();
+		List<string> getHeaderParameterNames();
+		String getHeaderParameterValue(String name);
+		List<string> getParameterNames();
+		string getParameterValue(String name);
+	}
+
+	public interface Response {
+		HttpStatusCode getStatus();
+		void setStatus(HttpStatusCode status);
+		DateTime getDate();
+		string getContent();
+		void setContent(String content);
+		string toString();
+	}
+
+	public interface WebApplication {
+		void process(Request request, Response response);
+	}
+
+	public interface Cookie {
+		string getName();
+		void setName(String name);
+		string getValue();
+		void setValue(String value);
+	}
+}
+
---
+++ webserver-csharp/Webserver.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Net.Sockets;
+using System.IO;
+using System.Threading;
+
+namespace nl.sogyo.webserver
+{
+	public static class Webserver
+	{
+		public static void Main()
+		{
+			TcpListener listener = new TcpListener(9090);
+			listener.Start();
+			while (true)
+			{
+				Thread thread = new Thread(new ParameterizedThreadStart(HandleRequest));
+				thread.Start(listener.AcceptSocket());
+			}
+		}
+
+		static void HandleRequest(Object socket)
+		{
+			Socket ssocket = socket as Socket;
+			NetworkStream networkstream = new NetworkStream(ssocket);
+			StreamReader reader = new StreamReader(networkstream);
+			string input=null;
+			//read the input until a blank line is read
+			while (input != "")
+			{
+				input = reader.ReadLine();
+				Console.WriteLine(input);
+			}
+			reader.Close();
+			networkstream = new NetworkStream(ssocket);
+			StreamWriter writer = new StreamWriter(networkstream);
+			writer.WriteLine("Thank you for connecting!");
+			writer.WriteLine();
+			writer.Close();
+			networkstream.Close();
+			ssocket.Close();
+		}
+	}
+}
+
