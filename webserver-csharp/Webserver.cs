7b2bf428fe3bebaa65793bffe062465389703331
Ruurd Heuvel <rheuvel@sogyo.nl>
2019-02-18 15:39:37 +0100

    Some refactoring of webserver.cs

--- webserver-csharp/Webserver.cs
+++ webserver-csharp/Webserver.cs
@@ -3,8 +3,8 @@ using System.Net.Sockets;
 using System.IO;
 using System.Threading;
 
-namespace nl.sogyo.webserver
-{
+namespace nl.sogyo.webserver {
+
 	public static class Webserver {
 
 		public static void Main() {
@@ -18,35 +18,44 @@ namespace nl.sogyo.webserver
 
 		static void HandleRequest(Object socket) {
 			Socket ssocket = socket as Socket;
-			NetworkStream networkstream = new NetworkStream(ssocket);
-			StreamReader reader = new StreamReader(networkstream);
-			string line=null;
-            string input = "";
-			//read the input until a blank line is read
-			while (line != "") {
-				line = reader.ReadLine();
-                input += line + "\r\n";
-				Console.WriteLine(line);
-			}
-			reader.Close();
+            string input = ReadMessage(ssocket);
             RequestMessage httpRequest = RequestMessage.parse(input);
-
             ResponseMessage response = ProcessNormalRequest(httpRequest);
-
-			networkstream = new NetworkStream(ssocket);
-			StreamWriter writer = new StreamWriter(networkstream);
-			writer.Write(response);
-
-			writer.WriteLine();
-			writer.Close();
-			networkstream.Close();
+            SendMessage(ssocket, response);
 			ssocket.Close();
 		}
+
+        static string ReadMessage(Socket socket) {
+            NetworkStream networkStream = new NetworkStream(socket);
+            StreamReader reader = new StreamReader(networkStream);
+            string line = null;
+            string input = "";
+            //read the input until a blank line is read
+            while (line != "") {
+                line = reader.ReadLine();
+                input += line + "\r\n";
+                Console.WriteLine(line);
+            }
+            reader.Close();
+            networkStream.Close();
+            return input;
+        }
 
         static ResponseMessage ProcessNormalRequest(RequestMessage httpRequest) {
             HtmlDocument htmlDocument = new HtmlDocument(httpRequest);
             return new ResponseMessage(htmlDocument.ToString(), DateTime.Now, HttpStatusCode.OK);
         }
+
+        static void SendMessage(Socket socket, ResponseMessage response) {
+            NetworkStream networkStream = new NetworkStream(socket);
+            StreamWriter writer = new StreamWriter(networkStream);
+            writer.Write(response);
+            writer.WriteLine();
+            writer.Close();
+            networkStream.Close();
+        }
+
 	}
+
 }
 
